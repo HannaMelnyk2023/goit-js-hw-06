e9e02140259aa71b0b15c67d775589480778b262
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2025-04-25 11:45:55 +0300

    corrected #6

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,10 @@
 const input = document.querySelector('#validation-input');
 input.addEventListener('blur', onValidate);
-
+console.log(Number(input.dataset.length));
 function onValidate(evt) {
-    if (evt.currentTarget.value.length === Number(input.dataset.length)) {
-        input.classList.add('valid')
-    } else {
+    if (evt.currentTarget.value.length != Number(input.dataset.length)) {
         input.classList.add('invalid')
+    } else {
+        input.classList.add('valid')
     }
 }
