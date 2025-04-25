8dfd7ce6be3a500e43d9625f6a9aced30968a6bf
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2025-04-25 14:12:59 +0300

    add  "remove valid/invalid"

--- js/task-06.js
+++ js/task-06.js
@@ -3,8 +3,10 @@ input.addEventListener('blur', onValidate);
 console.log(Number(input.dataset.length));
 function onValidate(evt) {
     if (evt.currentTarget.value.length != Number(input.dataset.length)) {
-        input.classList.add('invalid')
+        input.classList.add('invalid');
+        input.classList.remove('valid');
     } else {
-        input.classList.add('valid')
+        input.classList.add('valid');
+        input.classList.remove('invalid');
     }
 }
